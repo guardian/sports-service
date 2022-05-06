bf07d1b9d80a0f6b8f28e35633728bd7004413d3
David Lawes <david.lawes@guardian.co.uk>
2022-05-06 16:51:51 +0100

    Unhappy path for invalid json in request body

--- src/main/scala/com/gu/sports/api/PAEventAPI.scala
+++ src/main/scala/com/gu/sports/api/PAEventAPI.scala
@@ -1,7 +1,7 @@
 package com.gu.sports.api
 
 import com.gu.sports.services.PAEventService
-import org.scalatra.Ok
+import org.scalatra.{Ok, BadRequest}
 
 class PAEventAPI(
   val paEventService: PAEventService
@@ -9,7 +9,11 @@ class PAEventAPI(
 
   // I'm guessing PA's webhook is expecting a POST endpoint
   post("/") {
-    paEventService.handleEvent(parse(request.body))
-    Ok()
+    parseOpt(request.body) match {
+      case Some(body) =>
+        paEventService.handleEvent(body)
+        Ok()
+      case _ => BadRequest("json format expected")
+    }
   }
 }
--- src/test/scala/com/gu/sports/api/PAEventAPISpec.scala
+++ src/test/scala/com/gu/sports/api/PAEventAPISpec.scala
@@ -1,7 +1,7 @@
 package com.gu.sports.api
 
 import com.gu.sports.services.PAEventService
-import org.json4s.JValue
+import org.json4s.JsonAST.JValue
 import org.json4s.jackson.JsonMethods.parse
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito._
@@ -11,15 +11,22 @@ import scala.concurrent.Future
 
 class PAEventAPISpec extends ScalatraFunSuite {
   val jsonString = """{ "json" : "string" }"""
+  val invalidJson = "invalidJson"
   val paEventService = mock(classOf[PAEventService])
   addServlet(new PAEventAPI(paEventService), "/sports-event")
+  when(paEventService.handleEvent(any[JValue])).thenReturn(Future.successful())
 
-  test("POST /sports-event on PAEventAPI should return status 200") {
-    when(paEventService.handleEvent(any[JValue])).thenReturn(Future.successful())
+  test("POST /sports-event should return status 200") {
     post("/sports-event", jsonString) {
       verify(paEventService, times(1)).handleEvent(parse(jsonString))
       status should equal (200)
     }
   }
 
+  test("POST /sports-event should return 400 for invalid json body") {
+    post("/sports-event", invalidJson) {
+      status should equal (400)
+      response.body should equal ("json format expected")
+    }
+  }
 }
